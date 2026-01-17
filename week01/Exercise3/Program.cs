50b6aeb4388de88e88e30a5a1c428eb6d6866998
Kevin Gonzalez Alvarez <kevinglez131100@email.com>
2026-01-17 17:37:09 -0600

    Assignments completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            
+            int guess = -1;
+            int numberOfGuesses = 0;
+            
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string userInput = Console.ReadLine();
+                guess = int.Parse(userInput);
+                
+                numberOfGuesses++;
+                
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+            
+            Console.WriteLine($"It took you {numberOfGuesses} guesses.");
+            
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+        }
+        
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
