0e7ff4531f4ac05a1b583290b34346c9fa684479
Kevin Gonzalez Alvarez <kevinglez131100@email.com>
2026-02-08 20:14:36 -0600

    W05 Team Activity: Mindfulness Design - Class stubs

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+public class Activity
+{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public void DisplayStartingMessage()
+    {
+    }
+
+    public void DisplayEndingMessage()
+    {
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+    }
+}
---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+public class ReflectingActivity : Activity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    public void Run()
+    {
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+
+    public void DisplayPrompt()
+    {
+    }
+
+    public void DisplayQuestions()
+    {
+    }
+}
